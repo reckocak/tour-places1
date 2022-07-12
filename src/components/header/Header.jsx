84b206b76e980bec5170026018ed1c6e72547d1e
baileyrec <kocakrecep68@gmail.com>
2022-07-12 16:41:27 +0200

    start

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.scss'
+
+
+const Header = () =>{
+    return(
+        <div className="header">
+        <h1>Popular Tours Places</h1>
+
+
+        </div>
+    )
+}
+
+export default Header;
